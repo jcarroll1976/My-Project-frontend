9815bd0aebe9f32b6290683baf78c68579e372b0
Joshua Carroll <carrolljosh1976@gmail.com>
2022-09-27 18:01:17 -0400

    Continued working on inventory component

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -3,6 +3,12 @@ import "./Inventory.css";
 
 export default function Inventory() {
     return (
-        <img src="	https://mdencdn.com/thumbnail/prod_images/BL2110.JPG" alt="Blue84 University of Michigan Football Big Ten Champions Gray Tee"></img>
+        <div className="Inventory">
+        <img src="	https://mdencdn.com/thumbnail/prod_images/BL2110.JPG"
+         alt="Blue84 University of Michigan Football Big Ten Champions Gray Tee"></img>
+         <img src="https://mdencdn.com/thumbnail/prod_images/BL2106.JPG" 
+         alt="Blue84 University of Michigan Football Big Ten Champions Navy Locker Room Tee"></img>
+         <img src="	https://mdencdn.com/thumbnail/prod_images/NB1662.JPG" alt="Nike University of Michigan Navy Basic Dri-FIT Legend Tee"></img>
+         </div>
     )
 }

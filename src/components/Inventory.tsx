dc770fbdf9077c5457e33206295a5f963bcea39f
Joshua Carroll <carrolljosh1976@gmail.com>
2022-09-30 19:58:27 -0400

    added image to Inventory component

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -9,6 +9,8 @@ export default function Inventory() {
          <img src="https://mdencdn.com/thumbnail/prod_images/BL2106.JPG" 
          alt="Blue84 University of Michigan Football Big Ten Champions Navy Locker Room Tee"></img>
          <img src="	https://mdencdn.com/thumbnail/prod_images/NB1662.JPG" alt="Nike University of Michigan Navy Basic Dri-FIT Legend Tee"></img>
+         <img src="https://mdencdn.com/thumbnail/prod_images/NB1663.JPG"
+         alt="Nike University of Michigan Yellow Basic Dri-FIT Legend Tee"></img>
          </div>
     )
 }
